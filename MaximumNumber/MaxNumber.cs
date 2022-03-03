c1fc86d6a3be4f6ae1ad077d3a8569a57a67515f
saifalishaikh31 <rockyasaifalishaikh@gmail.com>
2022-03-03 14:17:15 +0530

    [Saif Ali] Add : Extend the max method to take more than three parameters Added

--- MaximumNumber/MaxNumber.cs
+++ MaximumNumber/MaxNumber.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,17 +9,21 @@ namespace MaximumNumber
 {
     public class MaxNumber<T> where T : IComparable
     {
-        public T FindMax(T firstStr, T secondStr, T thirdStr)
+        public T FindMax(T firstStr, T secondStr, T thirdStr, [Optional] T fourthStr)
         {
-            if (firstStr.CompareTo(secondStr) > 0 && firstStr.CompareTo(thirdStr) > 0)
+            if (firstStr.CompareTo(secondStr) > 0 && firstStr.CompareTo(thirdStr) > 0 && firstStr.CompareTo(fourthStr) > 0)
             {
                 return firstStr;
             }
-            if (secondStr.CompareTo(firstStr) > 0 && secondStr.CompareTo(thirdStr) > 0)
+            if (secondStr.CompareTo(firstStr) > 0 && secondStr.CompareTo(thirdStr) > 0 && secondStr.CompareTo(fourthStr) > 0)
             {
                 return secondStr;
             }
-            return thirdStr;
+            if (thirdStr.CompareTo(firstStr) > 0 && thirdStr.CompareTo(secondStr) > 0 && thirdStr.CompareTo(fourthStr) > 0)
+            {
+                return thirdStr;
+            }
+            return fourthStr;
         }
     }
 }
--- MaximumNumberTest/MaxNumberTest.cs
+++ MaximumNumberTest/MaxNumberTest.cs
@@ -11,63 +11,63 @@ namespace MaximumNumberTest
         [Test]
         public void GivenThreeIntNumFirstPosition_WhenChecked_ThenShouldReturnMax()
         {
-            int maximumInt = maxInt.FindMax(3, 2, 1);
-            Assert.AreEqual(3, maximumInt);
+            int maximumInt = maxInt.FindMax(4,3, 2, 1);
+            Assert.AreEqual(4, maximumInt);
         }
 
         [Test]
         public void GivenThreeIntNumSecondPosition_WhenChecked_ThenShouldReturnMax()
         {
-            int maximumInt = maxInt.FindMax(2, 3, 1);
-            Assert.AreEqual(3, maximumInt);
+            int maximumInt = maxInt.FindMax(2, 4, 3, 1);
+            Assert.AreEqual(4, maximumInt);
         }
 
         [Test]
         public void GivenThreeIntNumThirdPosition_WhenChecked_ThenShouldReturnMax()
         {
-            int maximumInt = maxInt.FindMax(2, 1, 3);
-            Assert.AreEqual(3, maximumInt);
+            int maximumInt = maxInt.FindMax(2, 1, 4, 3);
+            Assert.AreEqual(4, maximumInt);
         }
 
         [Test]
         public void GivenThreeFloatNumFirstPosition_WhenChecked_ThenShouldReturnMax()
         {
-            float maximumFloat = maxFloat.FindMax(5.0f, 1.3f, 2.2f);
-            Assert.AreEqual(5.0, maximumFloat);
+            float maximumFloat = maxFloat.FindMax(6.0f, 5.0f, 1.3f, 2.2f);
+            Assert.AreEqual(6.0, maximumFloat);
         }
 
         [Test]
         public void GivenThreeFloatNumSecondPosition_WhenChecked_ThenShouldReturnMax()
         {
-            float maximumFloat = maxFloat.FindMax(1.3f, 5.0f, 2.2f);
-            Assert.AreEqual(5.0, maximumFloat);
+            float maximumFloat = maxFloat.FindMax(1.3f, 6.0f, 5.0f, 2.2f);
+            Assert.AreEqual(6.0, maximumFloat);
         }
 
         [Test]
         public void GivenThreeFloatNumThirdPosition_WhenChecked_ThenShouldReturnMax()
         {
-            float maximumFloat = maxFloat.FindMax(2.2f, 1.3f, 5.0f);
-            Assert.AreEqual(5.0, maximumFloat);
+            float maximumFloat = maxFloat.FindMax(2.2f, 1.3f, 6.0f, 5.0f);
+            Assert.AreEqual(6.0, maximumFloat);
         }
         [Test]
         public void GivenThreeStringFirstPosition_WhenChecked_ThenShouldReturnMax()
         {
-            string maximumStr = maxStr.FindMax("Peach", "Apple", "Banana");
-            Assert.AreEqual("Peach", maximumStr);
+            string maximumStr = maxStr.FindMax("WaterMelon", "Peach", "Apple", "Banana");
+            Assert.AreEqual("WaterMelon", maximumStr);
         }
 
         [Test]
         public void GivenThreeStringSecondPosition_WhenChecked_ThenShouldReturnMax()
         {
-            string maximumStr = maxStr.FindMax("Apple", "Peach", "Banana");
-            Assert.AreEqual("Peach", maximumStr);
+            string maximumStr = maxStr.FindMax("Apple", "WaterMelon", "Peach", "Banana");
+            Assert.AreEqual("WaterMelon", maximumStr);
         }
 
         [Test]
         public void GivenThreeStringThirdPosition_WhenChecked_ThenShouldReturnMax()
         {
-            string maximumStr = maxStr.FindMax("Apple", "Banana", "Peach");
-            Assert.AreEqual("Peach", maximumStr);
+            string maximumStr = maxStr.FindMax("Apple", "Banana", "WaterMelon", "Peach");
+            Assert.AreEqual("WaterMelon", maximumStr);
         }
     }
 }
